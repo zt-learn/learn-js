8281b02d16ed625d24d894c256f6eb79aa74641a
zhentaoo <1904974327@qq.com>
2015-07-30 19:45:19 +0800

    插件

--- SlidePushMenus/js/classie.js
+++ SlidePushMenus/js/classie.js
@@ -14,42 +14,21 @@
 
     'use strict';
 
-// class helper functions from bonzo https://github.com/ded/bonzo
-
     function classReg(className) {
         return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
     }
 
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
     var hasClass, addClass, removeClass;
 
-    console.log(document.documentElement);
-    document.bgColor = 'gray';
-    if ('classList' in document.documentElement) {
-        hasClass = function (elem, c) {
-            return elem.classList.contains(c);
-        };
-        addClass = function (elem, c) {
-            elem.classList.add(c);
-        };
-        removeClass = function (elem, c) {
-            elem.classList.remove(c);
-        };
-    }
-    else {
-        hasClass = function (elem, c) {
-            return classReg(c).test(elem.className);
-        };
-        addClass = function (elem, c) {
-            if (!hasClass(elem, c)) {
-                elem.className = elem.className + ' ' + c;
-            }
-        };
-        removeClass = function (elem, c) {
-            elem.className = elem.className.replace(classReg(c), ' ');
-        };
-    }
+    hasClass = function (elem, c) {
+        return elem.classList.contains(c);
+    };
+    addClass = function (elem, c) {
+        elem.classList.add(c);
+    };
+    removeClass = function (elem, c) {
+        elem.classList.remove(c);
+    };
 
     function toggleClass(elem, c) {
         var fn = hasClass(elem, c) ? removeClass : addClass;
